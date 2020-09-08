7153b64950178058cf75113adabc51843a604e86
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2020-09-08 23:37:18 +0200

    Add 7zip

--- notebook_installer.ps1
+++ notebook_installer.ps1
@@ -6,4 +6,22 @@ write-output "# ----------------------"
 
 write-output ""
 
+if (Get-Command scoop -errorAction SilentlyContinue) {
+  "Scoop is already installed."
+} else {
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+  scoop bucket add extras
+  scoop bucket add nonportable
+}
+
+write-output ""
+
+if (Get-Command scoop install 7zip -errorAction SilentlyContinue) {
+  "7zip is already installed."
+} else {
+  scoop install 7zip
+}
+
+write-output ""
+
 read-host "Press ENTER to continue..."
