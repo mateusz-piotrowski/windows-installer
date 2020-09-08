69be353956b0e583ca915b3cbff2bb40b71b8d7c
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2020-09-08 22:21:27 +0200

    Add header

---
+++ notebook_installer.ps1
@@ -0,0 +1,9 @@
+write-output ""
+
+write-output "# ----------------------"
+write-output "# The Notebook Installer"
+write-output "# ----------------------"
+
+write-output ""
+
+read-host "Press ENTER to continue..."
