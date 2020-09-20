b8bac2cf982fd577662988b9ad871a90352edc15
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2020-09-20 20:30:54 +0200

    Enable 'Virtual Machine Platform' feature

--- wsl2_setup.ps1
+++ wsl2_setup.ps1
@@ -3,6 +3,9 @@
 ""
 
 "Enable 'Windows Subsystem for Linux' feature."
-
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
+""
+
+"Enable the 'Virtual Machine Platform' feature"
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
