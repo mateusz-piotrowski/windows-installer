b9d3a1c5dcff160e1e88a407f3750b80e3f0121b
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2023-06-28 00:09:43 +0200

    Add header

---
+++ windows-installer.ps1
@@ -0,0 +1,10 @@
+""
+
+Write-Host "# ----------------------"  -ForegroundColor DarkCyan
+Write-Host "# The Windows Installer"   -ForegroundColor DarkCyan
+Write-Host "# ----------------------"  -ForegroundColor DarkCyan
+
+# System colors:  Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray,
+# | Blue, Green, Cyan, Red, Magenta, Yellow, White"
+
+""
